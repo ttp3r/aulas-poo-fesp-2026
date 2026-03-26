71e3f7a21c71bcb9796bbbedd804e18943e92071
Talita <89655554+ttp3r@users.noreply.github.com>
2026-03-25 21:20:55 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+
+namespace SISTEMADEALUNO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o nome do aluno");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite a primeira nota");
+            double nota1 = double.Parse(
+                Console.ReadLine(),
+                CultureInfo.InvariantCulture
+            );
+
+            Console.WriteLine("Digite a segunda nota: ");
+            double nota2 = double.Parse(
+                Console.ReadLine(),
+                CultureInfo.InvariantCulture
+            );
+
+
+            Aluno aluno = new Aluno(nome, nota1, nota2);
+            aluno.ExibirResultado();
+        }
+    }
+}
